6df755ac6da3b17372668dc1b788dd7e237f10ea
Thomas Kwashnak <shintaomonk@gmail.com>
2022-01-13 17:13:15 -0500

    added store to credential.helper

--- scripts/gitconfig.sh
+++ scripts/gitconfig.sh
@@ -1,3 +1,3 @@
-git config --global credential.helper
+git config --global credential.helper store
 git config --global user.name "Thomas Kwashnak"
 git config --global user.email "shintaomonk@gmail.com"
