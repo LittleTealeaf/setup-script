455891cd8bacfed8ee3ccd2ae6e1ba7494e9e7e0
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-02-02 09:50:26 -0500

    Renamed run.sh

--- scripts/gitconfig.sh
+++ scripts/gitconfig.sh
@@ -1,3 +1,3 @@
 git config --global credential.helper store
 git config --global user.name "Thomas Kwashnak"
-git config --global user.email "shintaomonk@gmail.com"
+git config --global user.email "ThomasKwashnak@gmail.com"
--- setup.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-# APPLICATIONS="python3.9 python3 openjdk-17-jdk openjdk-17-jre texlive-full yakuake TLP"
