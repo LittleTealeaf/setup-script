9132bcf9886715b39d7ce8637fc5cea0a85cb4e6
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-02-02 09:46:20 -0500

    Added beginning of run script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-APPLICATIONS="python3.9 python3 openjdk-17-jdk openjdk-17-jre texlive-full yakuake TLP"
+# APPLICATIONS="python3.9 python3 openjdk-17-jdk openjdk-17-jre texlive-full yakuake TLP"
