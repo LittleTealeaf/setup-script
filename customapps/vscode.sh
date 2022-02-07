adfc541c8fc16a910326bc6ce799f35fe746cad0
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-07 15:44:54 -0500

    Made custom scripts simpler

--- customapps/vscode.sh
+++ customapps/vscode.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-cd tmp
 wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
 mv -u * "vscode.deb"
-cd ..
+sudo apt install ./vscode.deb
--- run.sh
+++ run.sh
@@ -9,9 +9,12 @@ sudo apt upgrade
 cat ./resources/aptlist.txt | xargs sudo apt -y install
 
 # Install application scripts in customapps
+cd tmp
 ls ./customapps | while read line; do
-    sudo bash "./customapps/$line"
+    sudo bash "../customapps/$line"
+    sudo rm -r *
 done
+cd ..
 
 ./scripts/gitconfig.sh
 
